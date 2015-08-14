fe8d4dbcf6873b22783ec59c47d0a102dc08fe52
V5787 (Course ID) <v5787@cam.ac.uk>
2015-08-14 10:48:54 +0100

    little dance

--- drone-backend.js
+++ drone-backend.js
@@ -11,11 +11,36 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+})
     .on("ready", fly);
     
 // Fly the bot
 function fly(robot) {
-
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.config('general:navdata_demo', 'TRUE');
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    bot.drone.left(0.1);
+    after(1*1000, function() {
+        bot.drone.left(0);
+    });
+    bot.drone.right(0.1);
+    after(1*1000, function() {
+        bot.drone.right(0);
+    });
+    after(10*1000, function() {
+        bot.drone.land();
+    });
+    after(15*1000, function() {
+        bot.drone.stop();
+    });
 }
 
 Cylon.start();
